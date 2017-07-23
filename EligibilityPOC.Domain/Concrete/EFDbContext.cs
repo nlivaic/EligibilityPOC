057df5f246b90682673fc278d96d7c3a652db1e1
nlivaic <nlivaic@gmail.com>
2017-07-23 17:54:20 +0200

    Type: feat
    DbContext definition and connection string.

--- EligibilityPOC.Domain/Concrete/EFDbContext.cs
+++ EligibilityPOC.Domain/Concrete/EFDbContext.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using EligibilityPOC.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EligibilityPOC.Domain.Concrete {
-    class Class1 {
+    class EFDbContext : DbContext {
+        public DbSet<ProductData> ProductData { get; set; }
+        public DbSet<ProductEligibilityParam> ProductEligibilityParam { get; set; }
     }
 }
