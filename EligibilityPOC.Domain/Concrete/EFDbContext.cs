dae27bb4c7b3d9ed5992e6e8254fde8cb7d04598
nlivaic <nlivaic@gmail.com>
2017-07-23 20:09:22 +0200

    Type: fix
    Fix the "Provider not loaded" error that crops up on the WebUI project when using EF.

--- EligibilityPOC.Domain/Concrete/EFDbContext.cs
+++ EligibilityPOC.Domain/Concrete/EFDbContext.cs
@@ -10,5 +10,13 @@ namespace EligibilityPOC.Domain.Concrete {
     class EFDbContext : DbContext {
         public DbSet<ProductData> ProductData { get; set; }
         public DbSet<ProductEligibilityParam> ProductEligibilityParam { get; set; }
+
+            public EFDbContext() {
+                // This is a hack to ensure that Entity Framework SQL Provider is copied across to the output folder.
+                // As it is installed in the GAC, Copy Local does not work. It is required for probing.
+                // Fixed "Provider not loaded" error.
+                var ensureDLLIsCopied =
+                    System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+            }
     }
 }
