9f4ef26a9e96257b38915adaafd4d64ef6f9e015
nlivaic <nlivaic@gmail.com>
2017-07-23 19:40:12 +0200

    Type: feat
    Add product repository Entity Framework implementation.

---
+++ EligibilityPOC.Domain/Concrete/EFProductDataRepository.cs
@@ -0,0 +1,23 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EligibilityPOC.Domain.Entities;
+
+namespace EligibilityPOC.Domain.Concrete {
+    public class EFProductDataRepository : IProductDataRepository {
+        private EFDbContext context;
+
+        public EFProductDataRepository() {
+            context = new EFDbContext();
+        }
+
+        public IQueryable<ProductData> ProductData {
+            get {
+                return context.ProductData;
+            }
+        }
+    }
+}
