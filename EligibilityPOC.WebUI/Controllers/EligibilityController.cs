591d0f43cf5c0a3d3d52ef578e2e6bdbed8f074a
nlivaic <nlivaic@gmail.com>
2017-07-23 12:27:44 +0200

    Type: feat
    Eligibility controller.

---
+++ EligibilityPOC.WebUI/Controllers/EligibilityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EligibilityPOC.WebUI.Controllers
+{
+    public class EligibilityController : Controller
+    {
+        // GET: Eligibility
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
