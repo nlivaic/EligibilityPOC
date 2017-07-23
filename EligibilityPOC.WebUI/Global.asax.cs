5f695562c3d6372c5ef76ddc02072e6e1834d465
nlivaic <nlivaic@gmail.com>
2017-07-23 13:44:27 +0200

    Type: feat
    Register dependency resolver with WebUI project.

--- EligibilityPOC.WebUI/Global.asax.cs
+++ EligibilityPOC.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EligibilityPOC.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace EligibilityPOC.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
         }
     }
 }
