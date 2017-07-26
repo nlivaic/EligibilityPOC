a578c8513848ec190ce62285c616d2f9e33fb908
nlivaic <nlivaic@gmail.com>
2017-07-26 21:35:21 +0200

    Type: feat chore
    Add Product param repo.
    Register repo file with project.

---
+++ EligibilityPOC.Domain/Concrete/EFProductEligibilityParamRepository.cs
@@ -0,0 +1,17 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EligibilityPOC.Domain.Entities;
+
+namespace EligibilityPOC.Domain.Concrete {
+    public class EFProductEligibilityParamRepository : IProductEligibilityParamRepository {
+        private EFDbContext context = new EFDbContext();
+
+        public IQueryable<ProductEligibilityParam> GetProductEligibilityParams(int productId) {
+            return context.ProductEligibilityParam.Where(p => p.ProductId == productId);
+        }
+    }
+}
