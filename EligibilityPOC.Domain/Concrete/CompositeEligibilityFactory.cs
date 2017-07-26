ddf4bf5115a677b210c5d3af1915e6e6e126e529
nlivaic <nlivaic@gmail.com>
2017-07-26 22:30:10 +0200

    Type: feat
    Initial commit: composite product eligibility factory.
    Declared dependencies.
    No methods implemented yet.

---
+++ EligibilityPOC.Domain/Concrete/CompositeEligibilityFactory.cs
@@ -0,0 +1,20 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Concrete {
+    class CompositeEligibilityFactory : IEligibilityFactory {
+        private IProductEligibilityParamRepository prodEligRepository;
+
+        public CompositeEligibilityFactory(IProductEligibilityParamRepository prodEligRepo) {
+            prodEligRepository = prodEligRepo;
+        }
+
+        public IEligible Create(int productId) {
+            throw new NotImplementedException();
+        }
+    }
+}
