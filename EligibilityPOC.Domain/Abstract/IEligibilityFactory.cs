fdd58f4cd5e74b11a355cd60e94b1a7fe049dabe
nlivaic <nlivaic@gmail.com>
2017-07-26 22:13:09 +0200

    Type: feat
    Define eligibility factory interface.

---
+++ EligibilityPOC.Domain/Abstract/IEligibilityFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Abstract {
+    public interface IEligibilityFactory {
+        IEligible Create(int productId);
+    }
+}
