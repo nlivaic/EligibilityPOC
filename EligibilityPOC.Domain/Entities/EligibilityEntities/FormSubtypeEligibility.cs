edff202b8c7324346d5bd940805b4c8de50c8f6a
nlivaic <nlivaic@gmail.com>
2017-07-27 21:49:26 +0200

    Type: feat
    Eligibility entities.

---
+++ EligibilityPOC.Domain/Entities/EligibilityEntities/FormSubtypeEligibility.cs
@@ -0,0 +1,18 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Entities.EligibilityEntities {
+    public class FormSubtypeEligibility : IEligible {
+        public FormSubtypeEligibility() {
+
+        }
+
+        public bool IsEligible() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EligibilityPOC.Domain/Entities/EligibilityEntities/MultipleItemsEligibility.cs
@@ -0,0 +1,17 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Entities.EligibilityEntities {
+    public class MultipleItemsEligibility : IEligible {
+        public int MinCount { get; set; }
+        public int MaxCount { get; set; }
+
+        public bool IsEligible() {
+            throw new NotImplementedException();
+        }
+    }
+}
