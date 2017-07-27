9c4aa07cb3894cf3d6d724a021b9d5f0a48f1c8a
nlivaic <nlivaic@gmail.com>
2017-07-27 23:00:52 +0200

    Type: feat chore
    Add Null Eligibility object (implementing Null object pattern).
    Register new class file with project file.

---
+++ EligibilityPOC.Domain/Entities/EligibilityEntities/NullEligibility.cs
@@ -0,0 +1,14 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Entities.EligibilityEntities {
+    public class NullEligibility : IEligibility {
+        public bool IsEligible() {
+            return true;
+        }
+    }
+}
