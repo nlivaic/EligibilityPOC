748bf0de068f9fe0aec6b12d528af578d89e6208
nlivaic <nlivaic@gmail.com>
2017-07-29 21:03:27 +0200

    Type: docs
    Method description.

--- EligibilityPOC.Domain/Concrete/ActivatorProductParamToEligibilityMapper.cs
+++ EligibilityPOC.Domain/Concrete/ActivatorProductParamToEligibilityMapper.cs
@@ -8,6 +8,12 @@ using EligibilityPOC.Domain.Entities;
 
 namespace EligibilityPOC.Domain.Concrete {
     public class ActivatorProductParamToEligibilityMapper : IProductParamToEligibilityMapper {
+        /// <summary>
+        /// Takes basic list of eligibility parameters and transforms it into a list of eligibilities.
+        /// Multiple eligibility parameters might be necessary to create a single eligibility.
+        /// </summary>
+        /// <param name="eligParams">A list with a basic representation of eligibility parameters.</param>
+        /// <returns>A list of eligibilities, with no particular structure.</returns>
         public IList<IEligibility> MapParamsToEligibility(IList<ProductEligibilityParam> eligParams) {
             string eligibilityName = String.Empty;
             int ruleSet = 0;
