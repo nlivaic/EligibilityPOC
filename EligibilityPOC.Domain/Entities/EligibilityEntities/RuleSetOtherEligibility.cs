79766da5fb8534328ef5b423b60d4ae93a4e797a
nlivaic <nlivaic@gmail.com>
2017-07-30 18:26:42 +0200

    Type: feat chore
    Rule set eligibilities.
    Regsiter rule set eligibilities with Domain project file.

---
+++ EligibilityPOC.Domain/Entities/EligibilityEntities/RuleSetOtherEligibility.cs
@@ -0,0 +1,28 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Entities.EligibilityEntities {
+    /// <summary>
+    /// Composite IEligibility.
+    /// </summary>
+    public class RuleSetOtherEligibility : IEligibility {
+        public int RuleSet { get; set; }
+        public IList<IEligibility> Components { get; set; }
+
+        public RuleSetOtherEligibility() {
+            Components = new List<IEligibility>();
+        }
+
+        public bool IsEligible() {
+            throw new NotImplementedException();
+        }
+
+        public void AddComponent(IEligibility eligibility) {
+            Components.Add(eligibility);
+        }
+    }
+}
