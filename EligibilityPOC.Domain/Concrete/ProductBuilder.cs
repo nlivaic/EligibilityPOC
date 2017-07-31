37b378e4bb67426389d2c004020bbb11048302f8
nlivaic <nlivaic@gmail.com>
2017-07-31 23:00:40 +0200

    Type: feat
    Product builder implementing product builder interface - initial commit.

---
+++ EligibilityPOC.Domain/Concrete/ProductBuilder.cs
@@ -0,0 +1,15 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EligibilityPOC.Domain.Entities;
+
+namespace EligibilityPOC.Domain.Concrete {
+    public class ProductBuilder : IProductBuilder {
+        public Product Create(int productId) {
+            throw new NotImplementedException();
+        }
+    }
+}
