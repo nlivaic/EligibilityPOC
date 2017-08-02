dcbef38a6ed7e19d0e68ef38748a6d297900a3dd
nlivaic <nlivaic@gmail.com>
2017-08-02 23:28:15 +0200

    Type: feat refactor
    Pass product id through constructor. This way there is no need to check product id consistency across calls to build methods.

--- EligibilityPOC.Domain/Concrete/ProductBuilder.cs
+++ EligibilityPOC.Domain/Concrete/ProductBuilder.cs
@@ -10,22 +10,47 @@ namespace EligibilityPOC.Domain.Concrete {
     public class ProductBuilder : IProductBuilder {
         private IEligibilityFactory _eligibilityFactory;
         private IProductDataRepository _productDataRepository;
+        private int _productId;
+        private Product _product;
 
-        public ProductBuilder(IEligibilityFactory eligFactory, IProductDataRepository productDataRepo) {
+        public ProductBuilder(IEligibilityFactory eligFactory, IProductDataRepository productDataRepo, int productId) {
             _eligibilityFactory = eligFactory;
             _productDataRepository = productDataRepo;
-    }
-
-        public Product Create(int productId) {
-            ProductData productData = _productDataRepository.ProductDatas.Where(p => p.Id == productId).FirstOrDefault();
-            if (productData == null) {
-                return null;
-            }
-            Product product = new Product {
-                ProductData = productData,
-                Eligibility = _eligibilityFactory.Create(productId)
-            };
-            return product;
+            _productId = productId;
+            _product = new Product();
         }
+
+        /// <summary>
+        /// Build eligibility for the specified product.
+        /// </summary>
+        /// <returns>Builder itself.</returns>
+        public IProductBuilder BuildEligibility() {
+            ProductData productData = _productDataRepository.ProductDatas.Where(p => p.Id == _productId).FirstOrDefault();
+            _product.ProductData = productData;
+            return this;
+        }
+
+        /// <summary>
+        /// Build eligibility for the specified product.
+        /// </summary>
+        /// <param name="_productId"></param>
+        /// <returns>Builder itself.</returns>
+        public IProductBuilder BuildProductData() {
+            _product.Eligibility = _eligibilityFactory.Create(_productId);
+            return this;
+        }
+
+        /// <summary>
+        /// Returns fully built product, according to previously called build methods.
+        /// </summary>
+        /// <returns></returns>
+        public Product Build() {
+            // Missing product data means product either isn't constructed properly or it doesn't exist in the repo.
+            if (_product.ProductData == null) {
+                _product = null;
+            }
+            return _product;
+        }
+
     }
 }
