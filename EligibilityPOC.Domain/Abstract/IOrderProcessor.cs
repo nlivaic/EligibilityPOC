1b480b30ee427d05c6ec0053fec04e25e1150a08
nlivaic <nlivaic@gmail.com>
2017-08-03 22:31:49 +0200

    Type: refactor chore
    Renamed files.

---
+++ EligibilityPOC.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,15 @@
+﻿using EligibilityPOC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Abstract {
+    /// <summary>
+    /// Please note this interface is here simply to tie everything up.
+    /// </summary>
+    public interface IOrderProcessor {
+        void Process(Order order);
+    }
+}
---
+++ EligibilityPOC.Domain/Concrete/EligibilityOrderDirector.cs
@@ -0,0 +1,28 @@
+﻿using EligibilityPOC.Domain.Abstract;
+using EligibilityPOC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Concrete {
+    public class EligibilityOrderDirector : IOrderProcessor {
+        private IProductBuilder _builder;
+        private Product _product;
+        public EligibilityOrderDirector(IProductBuilder builder) {
+            _builder = builder;
+        }
+
+        private void Construct(int productId) {
+            _product = _builder.BuildProductData(productId).BuildEligibility().Build();
+        }
+
+        public void Process(Order order) {
+            foreach (int productId in order.Products) {
+                Construct(productId);
+                _product.Eligibility.IsEligible();
+            }
+        }
+    }
+}
