7354cd0b951ece2d4d54bb5e4a172d7661f2bc54
nlivaic <nlivaic@gmail.com>
2017-08-03 22:29:51 +0200

    Type: feat
    Order domain model.

---
+++ EligibilityPOC.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibilityPOC.Domain.Entities {
+    /// <summary>
+    /// Please note this class is not complete. It is here simply to facilitate IProductDirector, so we can tie everything up.
+    /// </summary>
+    public class Order {
+        public IList<int> Products { get; set; }
+
+        public Order(IList<int> products) {
+            Products = products;
+        }
+    }
+}
